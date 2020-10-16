271defd95c6742e5ae7590151215154a3cc1a98b
TendTo <casablancaernesto@gmail.com>
2020-10-16 20:45:53 +0200

    fix(db): use of bigint instead of varchar

--- data/db/meme_db_init.sql
+++ data/db/meme_db_init.sql
@@ -1,25 +1,25 @@
 /*Used to instantiate the database the first time*/
 CREATE TABLE IF NOT EXISTS pending_meme
 (
-  user_id varchar(20) NOT NULL,
-  u_message_id varchar(25) NOT NULL,
-  g_message_id varchar(25) NOT NULL,
-  group_id varchar(20) NOT NULL,
+  user_id BIGINT NOT NULL,
+  u_message_id BIGINT NOT NULL,
+  g_message_id BIGINT NOT NULL,
+  group_id BIGINT NOT NULL,
   PRIMARY KEY (group_id, g_message_id)
 );
 -----
 CREATE TABLE IF NOT EXISTS published_meme
 (
-  channel_id varchar(25) NOT NULL,
-  c_message_id varchar(25) NOT NULL,
+  channel_id BIGINT NOT NULL,
+  c_message_id BIGINT NOT NULL,
   PRIMARY KEY (channel_id, c_message_id)
 );
 -----
 CREATE TABLE IF NOT EXISTS votes
 (
-  user_id varchar(20) NOT NULL,
-  c_message_id varchar(25) NOT NULL,
-  channel_id varchar(20) NOT NULL,
+  user_id BIGINT NOT NULL,
+  c_message_id BIGINT NOT NULL,
+  channel_id BIGINT NOT NULL,
   is_upvote boolean NOT NULL,
   PRIMARY KEY (user_id, c_message_id, channel_id),
   FOREIGN KEY (c_message_id, channel_id) REFERENCES published_meme (c_message_id, channel_id) ON DELETE CASCADE ON UPDATE CASCADE
@@ -27,9 +27,9 @@ CREATE TABLE IF NOT EXISTS votes
 -----
 CREATE TABLE IF NOT EXISTS admin_votes
 (
-  admin_id varchar(20) NOT NULL,
-  g_message_id varchar(25) NOT NULL,
-  group_id varchar(20) NOT NULL,
+  admin_id BIGINT NOT NULL,
+  g_message_id BIGINT NOT NULL,
+  group_id BIGINT NOT NULL,
   is_upvote boolean NOT NULL,
   PRIMARY KEY (admin_id, g_message_id, group_id),
   FOREIGN KEY (g_message_id, group_id) REFERENCES pending_meme (g_message_id, group_id) ON DELETE CASCADE ON UPDATE CASCADE
@@ -37,13 +37,13 @@ CREATE TABLE IF NOT EXISTS admin_votes
 -----
 CREATE TABLE IF NOT EXISTS credited_users
 (
-  user_id varchar(20) NOT NULL,
+  user_id BIGINT NOT NULL,
   PRIMARY KEY (user_id)
 );
 -----
 CREATE TABLE IF NOT EXISTS banned_users
 (
-  user_id varchar(20) NOT NULL,
+  user_id BIGINT NOT NULL,
   PRIMARY KEY (user_id)
 );
 /*
